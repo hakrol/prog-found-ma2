20a6345ef2c96a21b600d458efac07ab3a2271c9
Håkon Rolfsen <hakonrrolfsen@gmail.com>
2020-01-09 12:45:22 +0100

    module delivery

---
+++ script.js
@@ -0,0 +1,37 @@
+//question 1
+var petArray = ["cat", "cow", "dog"];
+
+//question 2
+console.log(petArray[1]);
+
+//question 3
+petArray.push("sheep");
+
+//question 4
+console.log(petArray.length);
+
+//question 5
+var catObject = {
+  name: "kitty",
+  colour: "white with black dots",
+  age: "4"
+};
+
+//question 6
+console.log(catObject.age);
+
+//question 7
+var catArray = [catObject];
+
+//question 8
+for (var i = 0; i < catArray.length; i++) {
+  console.log(catArray[i]);
+}
+
+//question 9
+function logToConsole(value) {
+  console.log(value);
+}
+
+//question 10
+logToConsole("donkey");
